c5af1e89b96c39d9366118a9f2cedd6575403ded
William Boyer <w.boyer@outlook.com>
2024-07-09 21:43:51 +0100

    Updated README.md file, and added docstrings for all classes and methods.

--- README.md
+++ README.md
@@ -8,18 +8,26 @@
 
 
 ## Introduction
-
+This project is an Exploratory Data Analysis (EDA) of loan data. The following processes have been applied to the dataset:
+1. Convert Columns to Correct Format: Changes are made to certain columns of the dataset to make later operations easier.
+2. Handling NULL Values: Any NULL values are removed, either by dropping the row, dropping the column, or imputing the values from existing data.
+3. Skew Correction: Transformations are performed on certain overly-skewed columns, to create a more normal distribution of values.
+4. Handling Outliers: All columns are examined for outliers, which are then removed from the dataset.
+5. Dropping Co-linear Columns: Combinations of columns are evaluated to determine how highly-correlated they are, and redundant columns are removed from the dataset.
+6. Analysis and Visualisation: Finally, the resulting dataset is analysed to determine profits and losses.
 
 ## Installation Instructions
 You may download the contents of the repository from GitHub by either:
 - Using `git clone https://github.com/WillBoyer/exploratory-data-analysis---customer-loans-in-finance521.git`
 - Using the 'Download ZIP' button on the repository page.
 
-After downloading the files, navigate to the repository's root directory and run `python db_utils.py`
+After downloading the files, navigate to the repository's root directory and open `python data_exploration.ipynb` in Visual Studio Code.
+Then, click 'Run All' in the top bar of the `python data_exploration.ipynb` window to view all code output.
 
 ## File Structure
 .
 ├── `README.md`: The file you are currently reading. Gives explanations on the project, installation method, and licensing.
+├── `data_exploration.ipynb`: The Python notebook file that performs transformation and analysis on the dataset.
 └── `db_utils.py`: Contains a definition of the `RDSDatabaseConnector` class
 
 ## License Information
--- db_utils.py
+++ db_utils.py
@@ -8,16 +8,41 @@ from scipy import stats, special
 import plotly.graph_objects as go
 
 
-def get_credentials(file):
-    with open('credentials.yaml', 'r') as cred_file:
+def get_credentials(filename):
+    '''
+    Retrieves the credentials from the 'credentials.yaml' file,
+    which can then be used to connect to the database.
+    
+    Args:
+        filename (str): The YAML file that contains the database credentials.
+        
+    Returns:
+        dict: A Python dictionary of the database credentials.
+    '''
+    with open(filename, 'r') as cred_file:
         creds = yaml.safe_load(cred_file)['connection']
     return creds
 
 class RDSDatabaseConnector:
+    '''
+    Represents the connection to the database
+    
+    Attributes:
+        creds (dict): The credentials to connect to the database.
+    '''
     def __init__(self, credentials):
+        '''
+        See help(RDSDatabaseConnector) for accurate signature.
+        '''
         self.creds = credentials
         
     def create_conn(self):
+        '''
+        Uses the credentials to connect to the database.
+        
+        Returns:
+            sqlalchemy.engine.base.Engine: The output of the 'create_engine' function.
+        '''
         user = self.creds['RDS_USER']
         password = self.creds['RDS_PASSWORD']
         host = self.creds['RDS_HOST']
@@ -27,18 +52,51 @@ class RDSDatabaseConnector:
         return engine
     
     def get_dataframe(self, engine):
+        '''
+        Submits a query to the database, and stores the result a
+        Pandas dataframe.
+        
+        Args:
+            engine (sqlalchemy.engine.base.Engine): The database query engine.
+        
+        Returns:
+            pandas.DataFrame: The SQL database table, as converted to a Pandas Dataframe.
+        '''
         df = pd.read_sql_query('''SELECT * FROM loan_payments''', engine).set_index('id')
         return df
     
     def save_to_file(self, dataframe):
+        '''
+        Converts the Pandas dataframe into a CSV file.
+        
+        Args:
+            dataframe (pandas.Dataframe): The SQL database table, as converted to a Pandas Dataframe.
+        '''
         dataframe.to_csv('out.csv', index=False)
     
 class DataFrameInfo:
+    '''
+    This class contains several methods to gain information about the dataframe.
+    '''
     def describe(self, df):
+        '''
+        Lists the data types of all dataframe columns.
+        
+        Args:
+            df (pandas.Dataframe): The SQL database table, as converted to a Pandas Dataframe.
+        '''
         for c in df:
             print(f"Column '{c}' has data type {df[c].dtype}")
         
     def get_stats(self, df):
+        '''
+        Lists statistics for all columns of the dataframe.
+        For numerical data, this includes mean, median and standard deviation.
+        For all other data types, this is instead the mode of each column.
+        
+        Args:
+            df (pandas.Dataframe): The SQL database table, as converted to a Pandas Dataframe.
+        '''
         for c in df:
             print(f"Column '{c}':")
             # Stats will only work on numerical data
@@ -54,6 +112,13 @@ class DataFrameInfo:
                 print(f"\t Mode: {col_mode}")
         
     def count_distinct(self, df):
+        '''
+        Counts the numhber of unique values in each of the columns that
+        represent categorical data.
+        
+        Args:
+            df (pandas.Dataframe): The SQL database table, as converted to a Pandas Dataframe.
+        '''
         # Need to only analyse categorical data
         categorical_columns = ['term', 
                                'grade',
@@ -71,10 +136,24 @@ class DataFrameInfo:
             print(f"Column '{c}' has {no_distinct} unique values")
         
     def print_shape(self, df):
+        '''
+        Prints the number of rows and columns in the dataframe, in a user-
+        readable format.
+        
+        Args:
+            df (pandas.Dataframe): The SQL database table, as converted to a Pandas Dataframe.
+        '''
         shape = df.shape
         print(f"The dataframe has {shape[0]} rows and {shape[1]} columns")
     
     def count_null(self, df):
+        '''
+        Prints the number of null values in every column, both as a count and
+        a percentage of the total rows, in a human-readable format.
+        
+        Args:
+            df (pandas.Dataframe): The SQL database table, as converted to a Pandas Dataframe.
+        '''
         for c in df:
             column_name = c
             null_count = df[c].isna().sum()
@@ -83,11 +162,30 @@ class DataFrameInfo:
                 print(f"Column '{column_name}' has {null_count} null values ({null_percentage}% of total values)")
                 
     def get_skews(self, df):
+        '''
+        Prints the skew of all numerical columns in the dataframe, in a human-
+        readable format.
+        
+        Args:
+            df (pandas.Dataframe): The SQL database table, as converted to a Pandas Dataframe.
+        '''
         for column in df:
             if df[column].dtype != 'object' and column != 'member_id':
                 print(f"Skew of {column} is {df[column].skew()}")
                 
     def get_z_score(self, df, column):
+        '''
+        Creates a new dataframe for the specified column, containing the
+        z-score for each value in the column.
+        
+        Args:
+            df (pandas.Dataframe): The SQL database table, as converted to a Pandas Dataframe.
+            column (string): The name of the column to be analysed.
+            
+        Returns:
+            pandas.Dataframe: A dataframe with the input column, plus a column
+                showing the z-score for each of the values.
+        '''
         col_mean = df[column].mean()
         col_std = df[column].std()
         col_z_scores = (df[column] - col_mean) / col_std
@@ -96,6 +194,25 @@ class DataFrameInfo:
         return analysis_df
     
     def get_interquartile_range(self, df, column):
+        '''
+        Calculates the Interquartile Range (IQR) for a given column, prints it
+        in a user-readable format, and cuts down the dataframe to exclude all 
+        values which are either:
+            - Lower than Q1 by 1.5 * IQR
+            - Higher than Q3 by 1.5 * IQR
+        
+        The IQR is the range from the 1st quartile (Q1) to the 3rd quartile 
+        (Q3). If a value lies outside the range specified above, it is counted
+        as an outlier.
+        
+        Args:
+            df (pandas.Dataframe): The SQL database table, as converted to a Pandas Dataframe.
+            column (string): The name of the column to be analysed.
+            
+        Returns:
+            pandas.Dataframe: A cut-down dataframe, containing only the rows 
+                which contain outliers in the current column.
+        '''
         Q1 = df[column].quantile(0.25)
         Q3 = df[column].quantile(0.75)
         
@@ -106,8 +223,6 @@ class DataFrameInfo:
 
         # Identify outliers
         outliers = df[(df[column] < (Q1 - 1.5 * IQR)) | (df[column] > (Q3 + 1.5 * IQR))]
-        print("Outliers:")
-        print(outliers)
         return outliers
     
     def get_vif(self, df, column):
@@ -117,8 +232,17 @@ class DataTransform:
     None
 
 class Plotter:
+    '''
+    Produces plots to visualise aspects of the dataset such as skew.
+    '''
     @classmethod
     def show_all_skews(self, df, columns):
+        '''
+        For a given list of columns, produce histogram plots to visualise skew.
+        
+        Args:
+            columns (string[]): A list of column names to be analysed for skew.
+        '''
         sns.set(font_scale=0.7)
         f = pd.melt(df, value_vars=columns)
         g = sns.FacetGrid(f, col="variable",  col_wrap=3, sharex=False, sharey=False)
@@ -126,6 +250,16 @@ class Plotter:
             
     @classmethod
     def plot_pie_category(self, df, column):
+        '''
+        For a given column, produce a pie chart showing the percentage of rows
+        which have each value.
+        
+        Args:
+            column (string): The name of the column to be analysed.
+            
+        Returns:
+            plotly.graph_objects.Pie: The pie chart object.
+        '''
         categories = sorted([cat for cat in df[column].unique()])
         
         values = []        
@@ -138,70 +272,137 @@ class Plotter:
                               labels=categories)
         return category_pie        
     
-    # @classmethod
-    # def plot_pie_category(self, df, column, title):
-    #     categories = sorted([cat for cat in df[column].unique()])
-        
-    #     values = []        
-        
-    #     for c in categories:
-    #         count = len(df[df[column].str.contains(c)])
-    #         values.append(count)
-        
-    #     category_pie = px.pie(values=values, 
-    #                           names=categories, 
-    #                           title=title)
-    #     return category_pie
-    
 class DataFrameTransform:
+    '''
+    Contains methods to perform transforms on the dataset.
+    '''
     @classmethod
     def impute_with_median(self, df, column):
+        '''
+        Replaces NULL values with the median value of the column.
+
+        Args:
+            df (pandas.Dataframe): The SQL database table, as converted to a Pandas Dataframe.
+            column (string): The name of the column to be transformed.
+
+        Returns:
+            pandas.Dataframe: The transformed dataframe.
+        '''
         median = df[column].median()
-        # df[column].fillna(median, inplace=True)
         df.fillna({column: median}, inplace=True)
         return df
     
     @classmethod
     def impute_with_mean(self, df, column):
+        '''
+        Replaces NULL values with the mean value of the column.
+
+        Args:
+            df (pandas.Dataframe): The SQL database table, as converted to a Pandas Dataframe.
+            column (string): The name of the column to be transformed.
+
+        Returns:
+            pandas.Dataframe: The transformed dataframe.
+        '''
         mean = df[column].mean()
-        # df[column].fillna(mean, inplace=True)
         df.fillna({column: mean}, inplace=True)
         return df
 
     @classmethod
     def impute_with_mode(self, df, column):
+        '''
+        Replaces NULL values with the most common value of the column.
+
+        Args:
+            df (pandas.Dataframe): The SQL database table, as converted to a Pandas Dataframe.
+            column (string): The name of the column to be transformed.
+
+        Returns:
+            pandas.Dataframe: The transformed dataframe.
+        '''
         mode = df[column].mode()[0]
-        # df[column].fillna(mode, inplace=True)
         df.fillna({column: mode}, inplace=True)
         return df
 
     @classmethod
     def drop_null_rows(self, df, column):
+        '''
+        Drops all rows from the dataframe which have a NULL value in the given 
+        column.
+
+        Args:
+            df (pandas.Dataframe): The SQL database table, as converted to a Pandas Dataframe.
+            column (string): The name of the column containing the NULL values.
+
+        Returns:
+            pandas.Dataframe: The transformed dataframe.
+        '''
         df.dropna(axis=0, how='any', subset = column, inplace=True)
         return df
     
     @classmethod
     def log_transform(self, df, column):
+        '''
+        Performs a Log transform to correct skew in the given column of the
+        dataframe.
+
+        Args:
+            df (pandas.Dataframe): The SQL database table, as converted to a Pandas Dataframe.
+            column (string): The name of the column to be transformed.
+
+        Returns:
+            pandas.Dataframe: The transformed dataframe.
+        '''
         log_output = df[column]
         log_output = log_output.map(lambda i: np.log(i) if i > 0 else 0)
         return log_output
     
     @classmethod
     def box_cox_transform(self, df, column):
+        '''
+        Performs a Log transform to correct skew in the given column of the
+        dataframe.
+
+        Args:
+            df (pandas.Dataframe): The SQL database table, as converted to a Pandas Dataframe.
+            column (string): The name of the column to be transformed.
+
+        Returns:
+            pandas.Dataframe: The transformed dataframe.
+        '''
         boxcox_output = df[column]
-        # boxcox_output = boxcox_output.map(lambda i: stats.boxcox(i)[0])
         boxcox_output = boxcox_output.map(lambda i: special.boxcox1p(i, 0.25))
         return boxcox_output
     
     @classmethod
     def yeo_johnson_transform(self, df, column):
+        '''
+        Performs a Yeo-Johnson transform to correct skew in the given column of
+        the dataframe.
+
+        Args:
+            df (pandas.Dataframe): The SQL database table, as converted to a Pandas Dataframe.
+            column (string): The name of the column to be transformed.
+
+        Returns:
+            pandas.Dataframe: The transformed dataframe.
+        '''
         yeo_johnson_output = df[column]
-        # yeo_johnson_output = yeo_johnson_output.map(lambda i: np.log(i) if i > 0 else 0)
         yeo_johnson_output = yeo_johnson_output.map(lambda i: stats.yeojohnson(i))
         return yeo_johnson_output
     
     @classmethod
     def drop_outliers(self, df, column):
+        '''
+        Drops values from a dataframe column with a z-score of 3 or higher.
+
+        Args:
+            df (pandas.Dataframe): The SQL database table, as converted to a Pandas Dataframe.
+            column (string): The name of the column containing the outliers.
+        
+        Returns:
+            pandas.Dataframe: The transformed dataframe.
+        '''
         df = df[(np.abs(stats.zscore(df[column])) < 3)]
         return df
 
